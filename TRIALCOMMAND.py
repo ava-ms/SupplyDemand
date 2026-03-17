f76fb384a853c1c394586d240966ab91135b2e2d
ava-ms <ava.melaerts@gmail.com>
2026-03-17 14:44:53 -0500

    Delete TRIALCOMMAND.py

--- TRIALCOMMAND.py
+++
@@ -1,9 +0,0 @@
-cd C:\Users\melaea\SupplyDemand
-git init
-git config user.name "ava-ms"
-git config user.email "your-email@example.com"
-git remote add origin https://github.com/ava-ms/SupplyDemand.git
-git add .
-git commit -m "Initial commit - Supply/Demand Dashboard"
-git branch -M main
-git push -u origin main
