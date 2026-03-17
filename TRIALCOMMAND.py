f6b694826dd664e980fca5a0333e0da24550efde
ava-ms <your-email@example.com>
2026-03-17 09:41:29 -0500

    Initial commit - Supply/Demand Dashboard

---
+++ TRIALCOMMAND.py
@@ -0,0 +1,9 @@
+cd C:\Users\melaea\SupplyDemand
+git init
+git config user.name "ava-ms"
+git config user.email "your-email@example.com"
+git remote add origin https://github.com/ava-ms/SupplyDemand.git
+git add .
+git commit -m "Initial commit - Supply/Demand Dashboard"
+git branch -M main
+git push -u origin main
